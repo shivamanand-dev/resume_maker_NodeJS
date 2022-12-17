0050d9ced78725fdca18a56fac22c765a323a2e6
shivamanand-dev <shivam_anand@outlook.com>
2022-12-18 04:59:33 +0530

    signup completed

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,29 +1,62 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 const User = require("../../models/auth/User");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+const JWT_Secret = process.env.JWT_Secret;
 
-router.post("/signup", async (req, res) => {
-  try {
-    console.log(req.body);
+router.post(
+  "/signup",
+  [
+    body("email", "Enter correct email").isEmail(),
+    body("username", "Username must be min 3 char").isLength({ min: 3 }),
+    body("password", "Password must be min 6 char").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const { name, email, username, password } = req.body;
 
-    const { name, email, username, password } = req.body;
+      //   Handle Validators
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
 
-    //   Find user with same userName and email
-    let user = await User.findOne({ email: email });
-    // let availUsername = await User.findOne({ username: username });
+      //   Find user with same userName and email
+      let user = await User.findOne({ email: email });
+      let availUsername = await User.findOne({ username: username });
 
-    user = await User.create({
-      name: name,
-      email: email,
-      username: username,
-      password: password,
-    });
+      if (user || availUsername) {
+        return res.status(400).json({ message: "Already Exists" });
+      }
 
-    res.json(user);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ message: "Server error occur" });
+      //   Hash Password
+      var salt = bcrypt.genSaltSync(10);
+      var hashedPassword = bcrypt.hashSync(password, salt);
+
+      //   Create User
+      user = await User.create({
+        name,
+        email,
+        username,
+        password: hashedPassword,
+      });
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, JWT_Secret);
+
+      res.json({ user, authToken });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Server error occur" });
+    }
   }
-});
+);
 
 module.exports = router;
