8ddfcec0254d203e1a1a7a0f5a289ee8d9407c1d
shivamanand-dev <shivam_anand@outlook.com>
2022-12-18 06:38:08 +0530

    login completed

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+const connectMongo = require("./db");
+connectMongo();
+
 const express = require("express");
 const app = express();
+
+// CORS
+var cors = require("cors");
+app.use(cors());
 
 // Enviornment Variables
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT;
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+// Routes
+app.use(express.json());
+app.use("/auth", require("./routes/auth/auth"));
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
